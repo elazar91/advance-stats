f81a2de4acb35614f8533ec83bfd8de35e5960ec
Elazar Durlacher <elazar91@gmail.com>
2022-02-17 10:53:20 +0200

    add all player and team stats

--- src/components/advance-stats/team-stats/TeamStats.js
+++ src/components/advance-stats/team-stats/TeamStats.js
@@ -1,31 +1,25 @@
 const TeamStats = {
   // Possission
-  poss: (FGA, FTA, OReb, Oreb, OppDReb, FGM, TO) => {
-    return FGA + 0.44 * FTA - (1.07 * (OReb/Oreb+OppDReb)) * (FGA-FGM) + TO
-  },
+  poss: (FGA, FTA, OReb, Oreb, OppDReb, FGM, TO) => FGA + 0.44 * FTA - (1.07 * (OReb/Oreb+OppDReb)) * (FGA-FGM) + TO,
   // ORTG - Offencive Rating
-  ortg: (TPoss, OppPoss, Pts) => {
-    return 100 / (TPoss + OppPoss) * Pts
-  },
+  ortg: (TPoss, OppPoss, Pts) =>100 / (TPoss + OppPoss) * Pts,
   // DRTG - Defencive Rating
-  drtg: (TPoss, OppPoss, OppPts) => {
-    return 100 / (TPoss + OppPoss) * OppPts
-  },
-  // STL % = (Team STL/Opp Poss) * 100
-  // BLK % = (Team BLK / OppFGA-Opp 3FGA) * 100 
-  // Team AST % = (Team AST / Team FGM) * 100
-  // Assist Value
-  // Team AST Ratio = (Team AST / (TFGA + 0.44. * TFTA + TTO)) * 100
-  // Team TO Ratio = (Team TO / (TFGA + 0.44. * TFTA + TTO)) * 100
-  // TOReb % = (TOreb / TOreb + OppDReb) * 100
-  // TDreb % = (TDreb / TDreb + OppOReb) * 100
-  // TTReb % = (TTReb / Treb + OppTReb) * 100
-
+  drtg: (TPoss, OppPoss, OppPts) => 100 / (TPoss + OppPoss) * OppPts,
+  // STL%
+  stlp: (TSTL, OppPoss) => (TSTL/OppPoss) * 100,
+  // BLK%
+  blkp: (TBLK, OppFGA, OppTHREEFGA) => (TBLK / OppFGA-OppTHREEFGA) * 100,
+  // Team AST %
+  tast: (TAST, TFGM) => (TAST / TFGM) * 100,
   // Defensive Efficiency
-  // Offensive efficiency
+  dr: (TmPoss, OppPoss, OppPts) => 100 / (TmPoss + OppPoss) * OppPts,
+  // Offensive Efficiency
+  or: (TmPoss, OppPoss, Pts) => 100 / (TmPoss + OppPoss) * Pts,
   // NRGT - Net Rating
-  // Play
+  nrgt: (PTS, OppPTS, Poss) => 100 * ((PTS - OppPTS) / Poss),
   // Pace
+  pace: (minutes, TmPoss, OppPoss, TmMP) => minutes * ((TmPoss + OppPoss) / (2 * (TmMP / 5))),
+  // Play
   // 3PR - 3 Point Rate
   // FTR - Free Throw Rate
 };
