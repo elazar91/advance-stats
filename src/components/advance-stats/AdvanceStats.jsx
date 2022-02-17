a961f9afacd6a0d384e3c458f46eec1af40e5092
Elazar Durlacher <elazar91@gmail.com>
2022-02-17 15:32:00 +0200

    start player stats forms

--- src/components/advance-stats/AdvanceStats.jsx
+++ src/components/advance-stats/AdvanceStats.jsx
@@ -1,35 +1,33 @@
 import { useState } from "react";
+import PlayerStats from "./player-stats/PlayerStats";
 
 const AdvanceStats = () => {
   const [individualAssistsPercent, setIndividualAssistsPercent] = useState();
-  // const [individualReboundPercent, setIndividualReboundPercent] = useState();
-
-  function individualAssistPerc(assists, MP, TMP, TFGM, FGM) {
-    setIndividualAssistsPercent(
-      (100 * assists) / ((MP / (TMP / 5)) * TFGM - FGM)
-    );
-  }
+  const [individualTS, setIndividualTS] = useState();
 
   function submitHemdlerAssistsPerc(e) {
     e.preventDefault();
-    individualAssistPerc(
-      e.target.assists.value,
-      e.target.MP.value,
-      e.target.TMP.value,
-      e.target.TFGM.value,
-      e.target.FGM.value
+    setIndividualAssistsPercent(
+      PlayerStats.ast(
+        // e.target.assists.value,
+        e.target.MP.value,
+        e.target.TMP.value
+        // e.target.TFGM.value,
+        // e.target.FGM.value
+      )
     );
   }
 
-  // function individualReboundPerc(rebounds, TR, OPTR, MP, TMP) {
-  //   setIndividualReboundPercent(
-  //     (100 * (rebounds * (TMP / 5))) / (MP * (TR + OPTR))
-  //   );
-  // }
+  function TSHeandler(e) {
+    e.preventDefault();
+    setIndividualTS(
+      PlayerStats.ts(e.target.PTS.value, e.target.FGA.value, e.target.FTA.value)
+    );
+  }
 
   return (
     <div>
-      <form
+      {/* <form
         className="general assists-perc"
         onSubmit={submitHemdlerAssistsPerc}
       >
@@ -55,13 +53,14 @@ const AdvanceStats = () => {
         <input type="number" name="FGM" id="FGM" placeholder="סלי שדה" />
         <input type="submit" />
       </form>
-      <div>{individualAssistsPercent}</div>
-      {/* <div>
-        <div onClick={() => individualReboundPerc(17, 46, 88, 30, 240)}>
-          Rebound Percentage
-        </div>
-        <div>{individualReboundPercent}</div>
-      </div> */}
+      <div>{individualAssistsPercent}</div> */}
+      <form onSubmit={TSHeandler}>
+        <input type="number" name="PTS" id="PTS" placeholder="נקודות" />
+        <input type="number" name="FGA" id="FGA" placeholder="זריקות מהשדה" />
+        <input type="number" name="FTA" id="FTA" placeholder="זריקות עונשין" />
+        <input type="submit" />
+      </form>
+      <div>{individualTS}</div>
     </div>
   );
 };
--- src/components/advance-stats/player-stats/PlayerStats.js
+++ src/components/advance-stats/player-stats/PlayerStats.js
@@ -1,48 +1,28 @@
 
 const PlayerStats = {
   //USG
-   usg: (FGA, FTA, TO, TMP, MP, TFGA, TFTA, TTO) => {
-    return (
+   usg: (FGA, FTA, TO, TMP, MP, TFGA, TFTA, TTO) => (
       (100 * ((FGA + 0.44 * FTA + TO) * (TMP / 5))) /
       (MP * (TFGA + 0.44 * TFTA + TTO))
-    );
-  },
+    ),
   //eFG
-   efg: (FGM, THREEPM, FGA) => {
-    return (FGM + 0.5 * THREEPM) / FGA;
-  },
+   efg: (FGM, THREEPM, FGA) => (FGM + 0.5 * THREEPM) / FGA,
   //%TS
-   ts: (PTS, FGA, FTA) => {
-    return PTS / (2 * (FGA + 0.44 * FTA));
-  },
+   ts: (PTS, FGA, FTA) => PTS / (2 * (FGA + 0.44 * FTA)),
   //%AST
-   ast: (Assists, MP, TMP, TFGM, FGM) => {
-    return (100 * Assists) / ((MP / (TMP / 5)) * TFGM - FGM);
-  },
+  //  ast: (Assists, MP, TMP, TFGM, FGM) => (100 * Assists) / ((MP / (TMP / 5)) * TFGM - FGM),
   //%TREB
-   treb: (TR, TMP, MP, TTR, OppTTR) => {
-    return (100 * (TR * (TMP / 5))) / (MP * (TTR + OppTTR));
-  },
+  //  treb: (TR, TMP, MP, TTR, OppTTR) => (100 * (TR * (TMP / 5))) / (MP * (TTR + OppTTR)),
   //%OREB
-   oreb: (OR, TMP, MP, TOR, OppTDR) => {
-    return (100 * (OR * (TMP / 5))) / (MP * (TOR + OppTDR));
-  },
+  //  oreb: (OR, TMP, MP, TOR, OppTDR) => (100 * (OR * (TMP / 5))) / (MP * (TOR + OppTDR)),
   //%DREB
-   dreb: (DR, TMP, MP, TDR, OppTOR) => {
-    return (100 * (DR * (TMP / 5))) / (MP * (TDR + OppTOR));
-  },
+  //  dreb: (DR, TMP, MP, TDR, OppTOR) =>(100 * (DR * (TMP / 5))) / (MP * (TDR + OppTOR)),
   //%BLK
-   blk: (BLK, TMP, MP, OPPFGA, OPPTHREEPA) => {
-    return (100 * (BLK * (TMP / 5))) / (MP * (OPPFGA - OPPTHREEPA));
-  },
+  //  blk: (BLK, TMP, MP, OPPFGA, OPPTHREEPA) => (100 * (BLK * (TMP / 5))) / (MP * (OPPFGA - OPPTHREEPA)),
   //%STL
-   stl: (STL, TMP, MP, OPPPOSS) => {
-    return (100 * (STL * (TMP / 5))) / (MP * OPPPOSS);
-  },
+  //  stl: (STL, TMP, MP, OPPPOSS) => (100 * (STL * (TMP / 5))) / (MP * OPPPOSS),
   //%TOV
-  tov: (PTOV, FGA, FTA, TTOV) => {
-    return 100 * PTOV / (FGA + 0.44 * FTA + TTOV)
-  }
+  // tov: (PTOV, FGA, FTA, TTOV) => 100 * PTOV / (FGA + 0.44 * FTA + TTOV)
 };
 
 export default PlayerStats;
